efb898c9fa4c3dd9f3cbd5fa9bf3c26a433a5d0b
Md Muhaiminul <muhai4959@gmail.com>
2025-02-19 09:59:14 +0600

    Initial Commit

---
+++ video-playerTests/video_playerTests.swift
@@ -0,0 +1,17 @@
+//
+//  video_playerTests.swift
+//  video-playerTests
+//
+//  Created by Vaasist van Strien on 19/02/2025.
+//
+
+import Testing
+@testable import video_player
+
+struct video_playerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
